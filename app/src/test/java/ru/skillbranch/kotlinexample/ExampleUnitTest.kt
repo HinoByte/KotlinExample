0a33e08c705fb358369ba30c3327204d967f1f48
Vladimir <znayka2005@gmail.com>
2020-01-09 11:36:11 +0300

    add 5 task

--- app/src/test/java/ru/skillbranch/kotlinexample/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/kotlinexample/ExampleUnitTest.kt
@@ -176,6 +176,7 @@ class ExampleUnitTest {
     fun import_user_csv_server_1() {
         val source = listOf("  John    Doe ;JohnDoe@yandex.ru;[B@7591083d:c6adb4becdc64e92857e1e2a0fd6af84;;")
         val result = UserHolder.importUsers(source)[0]
+
         val expectedInfo = """
             firstName: John
             lastName: Doe
